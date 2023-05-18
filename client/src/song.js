b38811727eefd61818505274d455f0dc742e6499
Gabriel Tidy <gabriel@bbd.co.za>
2023-05-18 11:28:38 +0200

    added random volume controller

--- client/src/song.js
+++ client/src/song.js
@@ -1,4 +1,5 @@
 const audio = document.getElementById("audioPlayer");
+const volumeLabel = document.getElementById("volumeLabel");
 // let volume = document.getElementById("volume-slider");
 
 function playSong() {
@@ -10,6 +11,11 @@ function pauseSong() {
 function stopSong() {
   audio.pause();
   audio.currentTime = 0;
+}
+
+function randomiseVolume() {
+  audio.volume = Math.random().toFixed(2);
+  volumeLabel.innerText = `Current volume: ${(audio.volume * 100).toFixed(0)}`;
 }
 
 // volume.addEventListener("input", function(e) {
