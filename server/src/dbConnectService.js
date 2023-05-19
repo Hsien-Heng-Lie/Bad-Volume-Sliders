a9d8e2923aa09f38ef1191f76f72f92909600525
Hsien Heng Lie <hsien@bbd.co.za>
2023-05-19 10:35:33 +0200

    updated config and rename dbConnectHandler to dbConnectService

--- server/src/dbConnectService.js
+++ server/src/dbConnectService.js
@@ -2,11 +2,14 @@ const sql = require("mssql/msnodesqlv8");
 const config = require('../../config.json');
 
 const connect = async () => {
+
   const pool = new sql.ConnectionPool({
+    user: config.sql.user,
+    password: config.sql.password,
     server: config.sql.dataSource,
     database: config.sql.databse,
     options: {
-      trustedConnection: true
+      trustedConnection: false
     }
   });
 
