a306a3844cfa91f529ef36c1c6aaba768fc2ef76
Kyle Fortuin <kylef@bbd.co.za>
2023-05-26 01:22:58 +0200

    fix ups and updates

--- client/js/imageBalancer.js
+++ client/js/imageBalancer.js
@@ -2,12 +2,15 @@
 const imageUpload = document.getElementById('image-upload');
 const imageCanvas = document.getElementById('image-canvas');
 const audioElement = document.getElementById('audio-player');
-const audioDisplay = document.getElementById('audio-display');
+
+// const audioDisplay = document.getElementById('audio-display');
 
 imageUpload.addEventListener('change', handleImageUpload);
 
 function handleImageUpload(event) {
   const file = event.target.files[0];
+  let volumeLabel = document.getElementById('volume-label');
+  let volumeSlider = document.getElementById('volume-slider');
 
   if (file) {
     // Read the file as data URL
@@ -32,8 +35,8 @@ function handleImageUpload(event) {
         // Set the volume based on the average RGB value
         const maxVolume = 100; // Adjust this value to set the maximum volume
         const volume = Math.round((averageRGB / 255) * maxVolume);
-        console.log(volume)
-        audioDisplay.innerHTML = volume
+        volumeSlider = volume
+        volumeLabel.innerHTML = `Current volume: ${volume}%`; 
         audioElement.volume = volume / maxVolume;
       };
       image.src = e.target.result;
