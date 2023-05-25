24bcda2d8a718c03e214e0ab8e8d0fbd5fbd8097
Hsien Heng Lie <hsien@bbd.co.za>
2023-05-26 00:06:24 +0200

    init ad

---
+++ client/js/ad.js
@@ -0,0 +1,36 @@
+const audioPlayer = document.getElementById('audio-player');
+const volumeLabel = document.getElementById('volume-label');
+
+const upButton = document.getElementById('volume-up');
+const downButton = document.getElementById('volume-down');
+
+
+// Add event listeners to the buttons
+upButton.addEventListener('click', () =>
+  incrementVolume()
+);
+
+downButton.addEventListener('click', () =>
+decrementVolume()
+);
+
+function incrementVolume() {
+  const currentVolume = audioPlayer.volume;
+  if(currentVolume < 1){
+    audioPlayer.volume += 0.01;
+  }
+  update();
+};
+
+function decrementVolume() {
+  const currentVolume = audioPlayer.volume;
+  if(currentVolume > 0){
+    audioPlayer.volume -= 0.01;
+  }
+  update();
+};
+
+function update(){
+  let currentVolume = audioPlayer.volume * 100;
+  volumeLabel.innerText = `Current volume: ${currentVolume.toFixed()}%`;
+}
